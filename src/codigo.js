fa20d7a777ff16f9fee254933bc9a4f0997b7120
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-08-02 21:19:22 -0300

    ...

--- src/codigo.js
+++ src/codigo.js
@@ -2,3 +2,21 @@ console.clear();
 
 alert("Bienvenidos");
 console.log("Adios");
+
+//
+
+//var textoSaludo = "Holas Mundo";
+
+//function saludar() {
+// console.clear();
+// alert(textoSaludo);
+// console.log("chau");
+//}
+
+//function saludar1() {
+// alert("u" + textoSaludo);
+
+//for (var i = 1; i <= 10; i++) {
+//  console.log(i);
+// }
+//}
