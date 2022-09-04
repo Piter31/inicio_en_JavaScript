5110ccef1dd35bf1bf26e111da683a760a9f0a1a
Your Name <you@example.com>
2022-09-04 19:39:04 -0300

    ...

--- src/codigo.js
+++ src/codigo.js
@@ -3,18 +3,17 @@ console.clear();
 alert("Bienvenidos");
 console.log("Adios");
 console.log("vuelva prontos - apu de los simpson");
-//var textoSaludo = "Holas Mundo";
+let textoSaludo = "Holas Mundo";
 
-//function saludar() {
-// console.clear();
-// alert(textoSaludo);
-// console.log("chau");
-//}
+function saludar(){
+    console.clear();
+    alert(textoSaludo);
+    console.log("chau");
+}
 
-//function saludar1() {
-// alert("u" + textoSaludo);
-
-//for (var i = 1; i <= 10; i++) {
-//  console.log(i);
-// }
-//}
+function saludar1(){
+    alert("u" + textoSaludo);
+    for (var i = 1; i <= 10; i++){
+    console.log(i);
+ }
+}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+//console.clear();
+
+alert("Bienvenidos");
+//console.log("Adios");
+//console.log("vuelva prontos - apu de los simpson");
+
+let textoSaludo: String = "Holas Mundo";
+
+
+function saludar(){
+    console.clear();
+    alert(textoSaludo);
+    console.log("chau");
+}
+
+function saludar1(){
+    alert("u" + textoSaludo);
+    for (var i = 1; i <= 10; i++){
+    console.log(i);
+ }
+}
+
